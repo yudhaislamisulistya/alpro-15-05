35e325a5bb5f617e451abe1d2ad184b47d9a41b9
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2024-09-30 15:38:34 +0700

    Refactor code to remove commented out sections and unused variables

---
+++ Pertemuan_02/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	// var panjang, lebar, tinggi, volume float32
+	// panjang = 10.5
+	// lebar = 5.5
+	// tinggi = 2.5
+	// volume = panjang * lebar * tinggi
+	// fmt.Println("Volume balok adalah", volume)
+
+	// var jumlahMahasiswa int8
+	// jumlahMahasiswa = -50
+	// fmt.Println("Jumlah mahasiswa adalah", jumlahMahasiswa)
+
+	namaMahasiswa := "Agus Harjkoko"
+	umur := 20
+	// fmt.Println("Nama saya adalah", namaMahasiswa)
+	// fmt.Println("Umur saya adalah", umur)
+	// fmt.Println("Alamat memori dari variabel namaMahasiswa adalah", &namaMahasiswa)
+}
---
+++ Pertemuan_02/main.md
@@ -0,0 +1,16 @@
+program Menghtiung Volume Lemari
+   Program ini dapat menghitung volume lemari dengan panjang, lebar, dan tinggi yang       diinputkan oleh user.
+
+kamus/deklarasi
+    panjang, lebar, tinggi: meter
+    volume: meter kubik
+
+algoritma
+    panjang <- Masukkan panjang dari user
+    lebar <- Masukkan lebar dari user
+    tinggi <- Masukkan tinggi dari user
+
+    volume <- panjang * lebar * tinggi
+
+    Tampilkan volume
+end program
